Fixed major calculation error in the first-guess and TOA conversion.

pplib.py
@@ -533,13 +533,17 @@ def fit_portrait(data, model, init_params, P=None, freqs=None, nu_ref=np.inf,
     duration = time.time() - start
     phi = results.x[0]
     DM = results.x[1]
+    print phi, DM, id
     nfeval = results.nfev
     return_code = results.status
     rcstring = RCSTRINGS["%s"%str(return_code)]
-    #If the fit fails...????
+    #If the fit fails...????  These don't seem to be great indicators of the
+    #fit failing
     if results.success is not True:
-        sys.stderr.write("Fit failed with return code %d -- %s; TOA is %s\n"
-                %(results.status, rcstring, id))
+        filenm = id[:-id[::-1].index("_")-1]
+        subint = id[-id[::-1].index("_"):]
+        sys.stderr.write("Fit failed with return code %d -- %s; %s subint %s\n"
+                %(results.status, rcstring, filenm, subint))
     if not quiet and results.success is True:
         sys.stderr.write("Fit succeeded with return code %d -- %s\n"
                 %(results.status, rcstring))
@@ -699,12 +703,13 @@ def DM_delay(DM, freq, freq2=np.inf, P=None):
 def DM_delay_offset(DM, P=None, nu_ref1=np.inf, nu_ref2=np.inf):
     """
     Caculates the (constant) relative delay between a signal dedispersed with
-    respect to nu_ref1 and nu_ref2.
+    respect to nu_ref1 and nu_ref2 (returns delay(nu_ref2) - delay(nu_ref1)).
     If P [sec] is provided, the offset is returned in phase units [rot],
     other wise units of [sec] are returned.
     DM should be in [cm**-3 pc]; nu_refs are [MHz].
     Default is no conversion.
     """
+    if P is None: P = 1.0
     diff = Dconst * DM * P**-1 * (nu_ref1**-2 - nu_ref2**-2)
     return diff
 

pptoas.py
@@ -153,8 +153,12 @@ class GetTOAs:
                 #freqsx = doppler_correct_freqs(freqsx, df)
                 #nu0 = doppler_correct_freqs(nu0, df)
                 ####################
+                #Initial guess; have to be careful, since the subints are 
+                #dedispersed at different nu_fit
                 if nn == 0:
-                    rot_port = rotate_portrait(subints.mean(axis=0)[0], 0.0,
+                    mean_port = np.transpose(weights.mean(axis=0) *
+                            np.transpose(subints.mean(axis=0)[0]))
+                    rot_port = rotate_portrait(mean_port, 0.0,
                             DM0, P, freqs, nu_fit)
                     #PSRCHIVE Dedisperses w.r.t. center of band, which is
                     #different, in general, from nu_fit; this results in an
@@ -163,6 +167,11 @@ class GetTOAs:
                     #Currently, first_guess ranges between +/- 0.5
                     phaseguess = first_guess(rot_port, model, nguess=1000)
                     DMguess = DM0
+                    phaseguess_0 = phaseguess
+                #I have to subtract the DM_delay_offset, by empirical trials...
+                phaseguess = phaseguess_0 - DM_delay_offset(DMguess, P,
+                        nu_fits[0], nu_fit)
+                #show_residual(rot_port)
                 #    if not quiet: print "Phase guess: %.8f ; DM guess: %.5f"%(
                 #            phaseguess, DMguess)
                 #The below else clause might not be a good idea if RFI or
@@ -297,8 +306,9 @@ class GetTOAs:
             phis = self.phis[dfi]
             phi_errs = self.phi_errs[dfi]
             DMs = self.DMs[dfi]
-            #Phase conversion (hopefully, the signs are correct):
-            phis += DM_delay_offset(DMs, Ps, nu_fits, nu_refs)
+            #Phase conversion (hopefully, the signs are correct)...
+            #...I have to subtract the DM_delay_offset, by empirical trials...
+            phis -= DM_delay_offset(DMs, Ps, nu_fits, nu_refs)
             toas = np.array([epochs[nn] + pr.MJD((phis[nn] *
                 Ps[nn]) / (3600 * 24.)) for nn in xrange(nsubx)])
             #Do errors change, like above?