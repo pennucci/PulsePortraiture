b59cd42826c74ad44fd2402695c0fc1219a7d8f3
pennucci <pennucci@virginia.edu>
2013-06-19 17:09:25 -0400

    rm directory...

--- MCtests/postMC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRATCHDIR=/scratch/ppmc    #needs to match in the submit script
-
-rsync -avuxP ${HOSTNAME}:${SCRATCHDIR}/*inf ${PBS_O_WORKDIR}
-rsync -avuxP ${HOSTNAME}:${SCRATCHDIR}/*pick ${PBS_O_WORKDIR}
-rm -rf %${SCRATCHDIR}
