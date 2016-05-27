update to example.py, debug/improve ppalign.py, new header email, fixed make_fake_pulsar bug with rotation

examples/example.py
@@ -24,13 +24,12 @@ noise_std = 2.75 #Noise level of the band, per subintegration [flux units]
 dDM_mean = 3e-4  #Add in random dispersion measure offsets with this mean value
 dDM_std = 2e-4   #Add in random dispersion measure offsets with this std
 dDMs = np.random.normal(dDM_mean, dDM_std, nfiles)
-#dDMs = np.zeros(nfiles) #Uncomment and set dDM_mean and dDM_std to zero for \
+#dDMs = np.zeros(nfiles) #Uncomment this line and comment previous line for \
                          #no injected dDMs
 scint = True     #Add random scintillation
-#Adding scattering to the fake data may slow down the fit:
-#t_scat and alpha will be read from the gmodel file if TAU is non-zero there.
-t_scat = 50e-6   #Add scattering with this timescale [s] w.r.t. nu0
-alpha = -4.0     #t_scat will follow a powerlaw with this spectral index
+#Scattering parameters will be read from the modelfile.
+#Adding/fitting scattering to the fake data may slow down the fit:
+fitscat = True   #Fit scattering timescale
 fitalpha = False #Fit the scattering index
 
 weights = np.ones([nsub, nchan]) #Change if you want to have an "RFI" mask
@@ -48,18 +47,22 @@ for ifile in range(nfiles):
             nsub=nsub, npol=npol, nchan=nchan, nbin=nbin, nu0=nu0, bw=bw,
             tsub=tsub, phase=0.0, dDM=dDMs[ifile], start_MJD=start_MJD,
             weights=weights, noise_stds=noise_std, scales=1.0,
-            dedispersed=False, t_scat=t_scat, alpha=alpha, scint=scint,
-            state="Coherence", obs="GBT", quiet=quiet)
+            dedispersed=False, scint=scint, state="Stokes", obs="GBT",
+            quiet=quiet)
     #NB: the input parfile for fake data cannot yet have binary parameters
 os.system('psredit -q -m -c rcvr:name="fake_rx" -c be:name="fake_be" example-*.fits')
 
-#If you wanted to add a bunch of datafiles together, you could use PSRCHIVE's
-#psradd to get a high SNR portrait.
-metafile = "example.meta"
-os.system("ls example-*.fits > %s"%metafile)
-print "Adding data archives..."
-outfile = "example.port"
-os.system("psradd -T -P -E %s -M %s -o %s"%(ephemeris, metafile, outfile))
+#Here we build an average portrait from the data
+if nfiles > 1:
+    import ppalign as pa
+    metafile = "example.meta"
+    os.system("ls example-*.fits > %s"%metafile)
+    outfile = "example.port"
+    print "Adding data archives..."
+    pa.align_archives(metafile=metafile, initial_guess="example-1.fits",
+            tscrunch=True, pscrunch=True, outfile=outfile, niter=1, quiet=True)
+    #...or you could use PSRCHIVE's psradd to get a high SNR portrait.
+    #os.system("psradd -T -P -E %s -M %s -o %s"%(ephemeris, metafile, outfile))
 
 #Now we want to "build" our gaussian model from the data
 print "Running ppgauss.py to fit a gaussian model..."
@@ -70,11 +73,10 @@ dp = pg.DataPortrait(datafile)
 #Have a look at the data you're fitting
 dp.show_data_portrait()
 #Fit a model; see ppgauss.py for all options
-dp.make_gaussian_model(ref_prof=(nu0, bw/4), tau=(t_scat * dp.nbin) / dp.Ps[0],
-        fixloc=True, fixscat=not(bool(t_scat)), fixalpha=not(fitalpha),
-        niter=3, fiducial_gaussian=True, writemodel=True,
-        outfile="example-fit.gmodel", model_name="example-fit",
-        residplot="example.png", quiet=False)
+dp.make_gaussian_model(ref_prof=(nu0, bw/4), fixloc=True, fixscat=not(fitscat),
+        fixalpha=not(fitalpha), niter=3, fiducial_gaussian=True,
+        writemodel=True, outfile="example-fit.gmodel", writeerrfile=True,
+        model_name="example-fit", residplot="example-fit.png", quiet=False)
 #You can always then continue iterations using the ppgauss option -I or by:
 #niter = # 
 #modelfile = example-fit.gmodel

ppalign.py
@@ -5,12 +5,12 @@
 #########
 
 #ppalign is a command-line program used to align homogeneous data (i.e. from
-#    the receiver, with the same center frequency, bandwidth, and number of
-#    channels).  This is useful for making averaged portraits to either pass to
-#    ppgauss.py with -M to make a Gaussian model, or to smooth and use as a
+#    the same receiver, with the same center frequency, bandwidth, and number
+#    of channels).  This is useful for making averaged portraits to either pass
+#    to ppgauss.py with -M to make a Gaussian model, or to smooth and use as a
 #    model with pptoas.py.
 
-#Written by Timothy T. Pennucci (TTP; pennucci@virginia.edu).
+#Written by Timothy T. Pennucci (TTP; pennucci@email.virginia.edu).
 
 #Need option for constant Gaussian initial guess.
 
@@ -51,8 +51,25 @@ def psrsmooth_archive(archive, options="-W"):
     psrsmooth_call = sub.Popen(shlex.split(psrsmooth_cmd))
     psrsmooth_call.wait()
 
-def align_archives(metafile, initial_guess, outfile=None, rot_phase=0.0,
-        place=None, niter=1, quiet=False):
+def check_if_Stokes(metafile):
+    """
+    Checks that archives are either have state 'Stokes'.
+
+    metafile is a file containing PSRFITS archive names to be averaged.
+    """
+    datafiles = [datafile[:-1] for datafile in open(metafile, "r").readlines()]
+    for datafile in datafiles:
+        vap_cmd = "vap -c state %s"%datafile
+        state = sub.Popen(shlex.split(vap_cmd), stdout=sub.PIPE
+                ).stdout.readlines()[1].split()[1]
+        if state == 'Stokes':
+            return True
+        else:
+            print "Archives do not all have state 'Stokes'."
+            return False
+
+def align_archives(metafile, initial_guess, tscrunch=False, pscrunch=True,
+        outfile=None, rot_phase=0.0, place=None, niter=1, quiet=False):
     """
     Iteratively align and average archives.
 
@@ -64,9 +81,16 @@ def align_archives(metafile, initial_guess, outfile=None, rot_phase=0.0,
 
     metafile is a file containing PSRFITS archive names to be averaged.
     initial_guess is the PSRFITS archive providing the initial alignment guess.
+    tscrunch=True will average the subintegrations; recommended unless there is
+        a reason to fit each subint individually.
+    pscrunch=False will average the available polarizations as well as average
+        intensity.  Alignment and weighting is still performed only via the
+        total intensity portrait.
     outfile is the name of the output archive; defaults to
         <metafile>.algnd.fits.
     rot_phase is an overall rotation to be applied to the final output archive.
+    place is a phase value at which to roughly place the peak pulse; it
+        overrides rot_phase.
     niter is the number of iterations to complete.  1-5 seems to work ok.
     quiet=True suppresses output.
 
@@ -77,6 +101,8 @@ def align_archives(metafile, initial_guess, outfile=None, rot_phase=0.0,
     vap_cmd = "vap -c nchan,nbin %s"%initial_guess
     nchan,nbin = map(int, sub.Popen(shlex.split(vap_cmd), stdout=sub.PIPE
             ).stdout.readlines()[1].split()[-2:])
+    if pscrunch: npol = 1
+    else: npol = 4
     model_data = load_data(initial_guess, dedisperse=True, dededisperse=False,
             tscrunch=True, pscrunch=True, fscrunch=False, rm_baseline=True,
             flux_prof=False, refresh_arch=True, return_arch=True, quiet=quiet)
@@ -84,23 +110,24 @@ def align_archives(metafile, initial_guess, outfile=None, rot_phase=0.0,
     count = 1
     while(niter):
         print "Doing iteration %d..."%count
-        nsub = 0
         load_quiet = quiet
-        aligned_port = np.zeros((nchan,nbin))
+        aligned_port = np.zeros((npol,nchan,nbin))
         total_weights = np.zeros((nchan,nbin))
         for ifile in xrange(len(datafiles)):
             data = load_data(datafiles[ifile], dedisperse=False,
-                    tscrunch=False, pscrunch=True, fscrunch=False,
+                    tscrunch=tscrunch, pscrunch=pscrunch, fscrunch=False,
                     rm_baseline=True, quiet=load_quiet)
             DM_guess = data.DM
             for isub in data.ok_isubs:
-                port = data.subints[isub,0,data.ok_ichans[isub]]
-                freqs = data.freqs[isub,data.ok_ichans[isub]]
-                model = model_port[data.ok_ichans[isub]]
-                #print freqs-model_data.freqs[0,data.ok_ichans[isub]]
+                ichans = np.intersect1d(data.ok_ichans[isub],
+                        model_data.ok_ichans[0])
+                port = data.subints[isub,0,ichans]
+                freqs = data.freqs[isub,ichans]
+                model = model_port[ichans]
+                #print freqs-model_data.freqs[0,ichans]
                 P = data.Ps[isub]
-                SNRs = data.SNRs[isub,0,data.ok_ichans[isub]]
-                errs = data.noise_stds[isub,0,data.ok_ichans[isub]]
+                SNRs = data.SNRs[isub,0,ichans]
+                errs = data.noise_stds[isub,0,ichans]
                 nu_fit = guess_fit_freq(freqs, SNRs)
                 rot_port = rotate_data(port, 0.0, DM_guess, P, freqs,
                         nu_fit)
@@ -121,36 +148,40 @@ def align_archives(metafile, initial_guess, outfile=None, rot_phase=0.0,
                     results.scale_errs = np.array([results.scale_error])
                     results.covariance = 0.0
                 weights = np.outer(results.scales / errs**2, np.ones(nbin))
-                aligned_port[data.ok_ichans[isub]] += weights * \
-                        rotate_data(port, results.phase, results.DM, P, freqs,
-                                results.nu_ref)
-                total_weights[data.ok_ichans[isub]] +=  weights
-                nsub += 1
+                for ipol in range(npol):
+                    aligned_port[ipol, ichans] += weights * \
+                            rotate_data(data.subints[isub,ipol,ichans],
+                                    results.phase, results.DM, P,freqs,
+                                    results.nu_ref)
+                total_weights[ichans] +=  weights
             load_quiet = True
-        aligned_port[np.where(total_weights > 0)[0]] /= \
-                total_weights[np.where(total_weights > 0)[0]]
-        model_port = aligned_port
+        for ipol in range(npol):
+            aligned_port[ipol, np.where(total_weights > 0)[0]] /= \
+                    total_weights[np.where(total_weights > 0)[0]]
+        model_port = aligned_port[0]
         niter -= 1
         count += 1
     if rot_phase:
         aligned_port = rotate_data(aligned_port, rot_phase)
     if place is not None:
-        prof = aligned_port.mean(axis=0)
+        prof = aligned_port[0].mean(axis=0)
         delta = prof.max() * gaussian_profile(len(prof), place, 0.0001)
         phase = fit_phase_shift(prof, delta).phase
         aligned_port = rotate_data(aligned_port, phase)
     arch = model_data.arch
     arch.tscrunch()
-    arch.pscrunch()
+    if pscrunch: arch.pscrunch()
     arch.set_dispersion_measure(0.0)
     for subint in arch:
         for ipol in xrange(model_data.arch.get_npol()):
             for ichan in xrange(model_data.arch.get_nchan()):
-                #subint.set_weight(ichan, weight)
                 prof = subint.get_Profile(ipol, ichan)
-                prof.get_amps()[:] = aligned_port[ichan]
+                prof.get_amps()[:] = aligned_port[ipol,ichan]
                 if total_weights[ichan].sum() == 0.0:
                     subint.set_weight(ichan, 0.0)
+                else:
+                    #subint.set_weight(ichan, weight)
+                    subint.set_weight(ichan, 1.0)
     arch.unload(outfile)
     if not quiet: print "\nUnloaded %s.\n"%outfile
 
@@ -171,7 +202,15 @@ if __name__ == "__main__":
                       default=None,
                       action="store", metavar="initial_guess",
                       dest="initial_guess",
-                      help="Archive containing initial alignment guess.  psradd is used if -I is not used. [default=None]")
+                      help="Archive containing initial alignment guess.  psradd is used if -I is not used.")
+    parser.add_option("-T", "--tscr",
+                      default=False,
+                      action="store_true", dest="tscrunch",
+                      help="Tscrunch archives for the iterations.  Recommended unless there is reason to keep subint resolution (may speed things up).")
+    parser.add_option("-p", "--poln",
+                      default=True,
+                      action="store_false", dest="pscrunch",
+                      help="Output averaged polarizations, not just total intensity.")
     parser.add_option("-o", "--outfile",
                       default=None,
                       action="store", metavar="outfile", dest="outfile",
@@ -183,7 +222,7 @@ if __name__ == "__main__":
     parser.add_option("-s", "--smooth",
                       default=False,
                       action="store_true", dest="smooth",
-                      help="Smooth the output average (second output archive) with psrsmooth -W. [default=False]")
+                      help="Output a second averaged archive, smoothed with psrsmooth -W. [default=False]")
     parser.add_option("-r", "--rot",
                       default=0.0,
                       action="store", metavar="phase", dest="rot_phase",
@@ -191,7 +230,7 @@ if __name__ == "__main__":
     parser.add_option("--place",
                       default=None,
                       action="store", metavar="place", dest="place",
-                      help="Roughly place pulse to be at the phase of the provided argument.  Overrides --rot. [default=None]")
+                      help="Roughly place pulse to be at the phase given.  Overrides --rot. [default=None]")
     parser.add_option("--niter",
                       action="store", metavar="int", dest="niter", default=1,
                       help="Number of iterations to complete. [default=1]")
@@ -209,13 +248,16 @@ if __name__ == "__main__":
 
     metafile = options.metafile
     initial_guess = options.initial_guess
+    tscrunch = options.tscrunch
+    pscrunch = options.pscrunch
     outfile = options.outfile
     palign = options.palign
     smooth = options.smooth
     rot_phase = np.float64(options.rot_phase)
-    place = np.float64(options.place)
-    if place is not None:
+    if options.place is not None:
         rot_phase=0.0
+        place = np.float64(options.place)
+    else: place = None
     niter = int(options.niter)
     quiet = options.quiet
 
@@ -225,12 +267,14 @@ if __name__ == "__main__":
         psradd_archives(metafile, outfile=tmp_file, palign=palign)
         initial_guess = tmp_file
         rm = True
-    align_archives(metafile, initial_guess=initial_guess, outfile=outfile,
-            rot_phase=rot_phase, place=place, niter=niter, quiet=quiet)
-    if smooth:
-        if outfile is None:
-            outfile = metafile + ".algnd.fits"
-        psrsmooth_archive(outfile, options="-W")
+    if check_if_Stokes(metafile) or pscrunch:
+        align_archives(metafile, initial_guess=initial_guess,
+                tscrunch=tscrunch, pscrunch=pscrunch, outfile=outfile,
+                rot_phase=rot_phase, place=place, niter=niter, quiet=quiet)
+        if smooth:
+            if outfile is None:
+                outfile = metafile + ".algnd.fits"
+            psrsmooth_archive(outfile, options="-W")
     if rm:
         rm_cmd = "rm -f %s"%tmp_file
         rm_call = sub.Popen(shlex.split(rm_cmd))

pplib.py
@@ -6,7 +6,7 @@
 #programs ppgauss and pptoas, as well as some additional functions used in our
 #wideband timing analysis.
 
-#Written by Timothy T. Pennucci (TTP; pennucci@virginia.edu).
+#Written by Timothy T. Pennucci (TTP; pennucci@email.virginia.edu).
 #Contributions by Scott M. Ransom (SMR) and Paul B. Demorest (PBD) 
 
 #########
@@ -2256,10 +2256,11 @@ def make_fake_pulsar(modelfile, ephemeris, outfile="fake_pulsar.fits", nsub=1,
     arch.set_ephemeris(ephemeris)
     #Now finally, fill in the data!
     #NB the different pols are not realistic: same model, same noise_stds
-    #If wanting to use PSRCHIVE's rotation scheme, uncomment the dedisperse and
-    #dededisperse lines, and set rotmodel = model.
-    arch.set_dedispersed(False)
-    arch.dededisperse()
+    #rotmodel is now set to the unrotated model, and all rotation done by
+    #PSRCHIVE since whether or not the PSRCHIVE configuration is set to use the
+    #barycentric correction will matter
+    arch.set_dedispersed(True)
+    arch.dedisperse()
     if weights is None: weights = np.ones([nsub, nchan])
     isub = 0
     (name, model_code, nu_ref, ngauss, params, fit_flags, scattering_index,
@@ -2270,11 +2271,14 @@ def make_fake_pulsar(modelfile, ephemeris, outfile="fake_pulsar.fits", nsub=1,
             name, ngauss, model = read_model(modelfile, phases, freqs, P,
                     quiet=True)
             if xs is None:
-                rotmodel = rotate_data(model, -phase, -(DM+dDM), P, freqs, nu0)
+                rotmodel = model
+                #rotmodel=rotate_data(model, -phase, -(DM+dDM), P, freqs, nu0)
             else:
                 phase = phase_transform(phase, DM+dDM, nu0, nu_DM, P)
-                rotmodel = add_DM_nu(model, -phase, -(DM+dDM), P, freqs, xs,
-                        Cs, nu_DM)
+                #rotmodel = add_DM_nu(model, -phase, -(DM+dDM), P, freqs, xs,
+                #        Cs, nu_DM)
+                rotmodel = add_DM_nu(model, -phase, -dDM, P, freqs, xs, Cs,
+                        nu_DM)
             #rotmodel = model
             if t_scat and not params[1]:    #modelfile overrides
                 sk = scattering_kernel(t_scat, nu0, freqs, phases, P,
@@ -2297,6 +2301,7 @@ def make_fake_pulsar(modelfile, ephemeris, outfile="fake_pulsar.fits", nsub=1,
                     prof.get_amps()[:] = scales[ichan]*rotmodel[ichan]
         isub += 1
     if dedispersed: arch.dedisperse()
+    else: arch.dededisperse()
     arch.unload(outfile)
     if not quiet: print "\nUnloaded %s.\n"%outfile
 