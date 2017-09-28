0cab6e49565a82966a6579c35c9a7273cf19eecc
Tim Pennucci <pennucci@email.virginia.edu>
2017-09-28 14:35:06 +0200

    Forgot to update setup.py before pushing ppzap.py.

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from distutils.core import setup
 
 setup(name='PulsePortraiture',
       version='0.0',
-      description='Data analysis package for wide-band pulsar timing',
+      description='Data analysis package for wideband pulsar timing',
       author='Tim Pennucci',
       author_email='tim.pennucci@nanograv.org',
       url='http://github.com/pennucci/PulsePortraiture',
       py_modules=['pplib'],
-      scripts=['ppalign.py','ppgauss.py', 'ppinterp.py', 'pptoas.py']
+      scripts=['ppalign.py','ppgauss.py', 'ppinterp.py', 'pptoas.py',
+          'ppzap.py']
      )
