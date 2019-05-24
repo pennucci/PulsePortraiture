1d1d509eb7bdf93b7722ca743b2b6a38f01b19d1
Tim Pennucci <pennucci@email.virginia.edu>
2019-05-24 15:33:50 +0200

    Fixed another subtle factor of 2 in cross_hess.

--- pptoaslib.py
+++ pptoaslib.py
@@ -635,7 +635,7 @@ def fit_portrait_full_function_2deriv(params, data_portrait_FT,
             ifit = np.where(fit_flags)[0] # which parameters are fit
             A = hessian[ifit].T[ifit].T
         scales_hess = np.identity(len(scales)) * 2*S
-        cross_hess = dC - scales*dS
+        cross_hess = -2*(dC - scales*dS)
         hessian = np.append(hessian, cross_hess, 1)
         hessian = np.append(hessian, np.append(cross_hess.T, scales_hess, 1),0)
         returns.append(hessian)
